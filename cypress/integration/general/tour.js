4bdfbb893f77504ce9b7203f3c21dfe274ec74ba
Jon <jdonesky@gmail.com>
2021-03-10 10:03:15 -0500

    initial commit

---
+++ cypress/integration/general/tour.js
@@ -0,0 +1,35 @@
+describe("make sure site loads", () => {
+  beforeEach(() => {
+    const configUrl =
+      "https://api.themoviedb.org/3/configuration?api_key=cc9872fea54d44034cda8029df86a696";
+    const moviesListUrl =
+      "https://api.themoviedb.org/3/discover/movie?language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&api_key=cc9872fea54d44034cda8029df86a696";
+    cy.intercept(configUrl, { fixture: "config" });  
+    cy.intercept(moviesListUrl, { fixture: "moviesList" });
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("Page loads", () => {
+    cy.findAllByTestId("movies-list-item")
+      .first()
+      .then(($movie) => {
+        const movieUrl = $movie.attr("href");
+        cy.get("[data-testid=movies-list-item]").first().click();
+        cy.url().should("include", movieUrl);
+      });
+  });
+
+  it("shows correct number of movies", () => {
+    cy.get("[data-testid=movies-list-item]").should("have.length", 20);
+  });
+
+  it("understands chainers", () => {
+    cy.get("[data-testid=movies-list-item]").should("not.exist");
+    cy.get("[data-testid=movies-loading-movie]").should("not.exist");
+
+    cy.fixture("moviesList").then((jsonData) => {
+      console.log(jsonData.results[0]);
+      expect(jsonData.results[0].title).to.eq("Tom & Jerry");
+    });
+  });
+});
